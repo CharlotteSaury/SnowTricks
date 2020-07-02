98f1826a9f18efe58a1ec4667e426477bfab64da
CharlotteSaury <saury.charlotte@wanadoo.fr>
2020-07-02 17:35:56 +0200

    trickList : add loadMore button functionnality w/ JS

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    var tricksPerPage = 5;
+    var tricks = $('div.trick-card-div');
+    $('#arrowUp').hide();
+
+    for (var i = tricksPerPage; i <= tricks.length - 1; i++) {
+        tricks[i].remove();
+    }
+
+    if (tricks.length >= tricksPerPage) {
+        $('#loadLessBtn').hide();
+    }
+
+
+    $('#loadMoreBtn').on('click', function (e) {
+        e.preventDefault();
+        tricksPerPage += 5;
+        for (var i = 0; i <= tricksPerPage -1; i++) {
+            $('#trickList').append(tricks[i]);
+        }
+        if (tricks.length <= tricksPerPage) {
+            $('#loadLessBtn').show();
+            $('#loadMoreBtn').hide();
+        }
+        if (tricksPerPage >= 15) {
+            $('#arrowUp').show();
+        }
+    });
+
+    $('#loadLessBtn').on('click', function (e) {
+        e.preventDefault();
+        tricksPerPage = 5;
+        for (var i = tricksPerPage; i <= tricks.length - 1; i++) {
+            tricks[i].remove();
+        }
+        $('#loadLessBtn').hide();
+        $('#loadMoreBtn').show();
+        $('#arrowUp').hide();
+    });
+});

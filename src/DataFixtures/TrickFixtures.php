0404cdc9ce3468c11cae1633e3655388e1fdd951
CharlotteSaury <saury.charlotte@wanadoo.fr>
2020-08-19 08:56:11 +0200

    add video to trick fixture

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -3,18 +3,46 @@
 namespace App\DataFixtures;
 
 use App\Entity\Trick;
-use App\DataFixtures\UserRoleFixtures;
+use App\Entity\Video;
 use App\DataFixtures\GroupFixtures;
+use App\Service\VideoLinkFormatter;
+use App\DataFixtures\UserRoleFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class TrickFixtures extends Fixture implements DependentFixtureInterface
 {
+    private $videoLinkFormatter;
+
+    public function __construct(VideoLinkFormatter $videoLinkFormatter)
+    {
+        $this->videoLinkFormatter = $videoLinkFormatter;
+    }
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create();
-
+        $videos = [
+            'http://www.youtube.com/watch?v=AzJPhQdTRQQ&t=1s',
+            'https://www.youtube.com/watch?v=axNnKy-jfWw',
+            'www.youtube.com/watch?v=axNnKy-jfWw',
+            'https://youtu.be/R2Cp1RumorU',
+            'https://youtu.be/UGdif-dwu-8',
+            'https://www.youtube.com/embed/M_BOfGX0aGs',
+            'https://www.dailymotion.com/video/x4b4ga',
+            'wwww.dailymotion.com/video/x2j4bgs',
+            'dailymotion.com/video/xwpx9p',
+            'https://dai.ly/xog7m7',
+            'dai.ly/x72qhs9',
+            'https://www.dailymotion.com/embed/video/x7vau0d',
+            'dailymotion.com/embed/video/x6xb7gd',
+            'https://vimeo.com/56415173',
+            'vimeo.com/151351853',
+            'https://vimeo.com/56688915',
+            'vimeo.com/159485768',
+            'http://vimeo.com/6097400',
+            'https://player.vimeo.com/video/17859252'
+        ];
 
         for ($i = 0; $i < 30; $i++) {
             $trick = new Trick();
@@ -32,12 +60,18 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
             foreach ($groups as $key => $value) {
                 $trick->addGroup($value);
             }
+
+            for ($k = 0; $k < mt_rand(1, 4); $k++) {
+                $video = new Video();
+                $formattedName = $this->videoLinkFormatter->format($videos[mt_rand(0, count($videos)-1)]);
+                $video->setName($formattedName);
+                $trick->addVideo($video);
+            }
                 
             $manager->persist($trick);
 
             $this->addReference('trick' . $i, $trick);
         }
-
 
         $manager->flush();
     }
